af4099654da80a85e8b797895e7862a1e6297cce
Daniel Tri <danielrudianto12@gmail.com>
2021-12-02 08:04:06 +0700

    Publish initial codes

---
+++ application/views/about.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Alpha Konstruksi Nusantara | Perusahaan Pengeboran Indonesia</title>
+	<meta name='description' content="PT Alpha Konstruksi Nusantara merupakan perusahaan kontraktor yang bergerak di bidang geoteknik. Spesialisasi kami adalah dalam melakukan pengeboran struktural.">
+	<meta name='keywords', content="Alpha Konstruksi Nusantara, kontraktor bor Indonesia">
+	<link rel="icon" href="<?= base_url() ?>assets/images/favicon.png" type="image/png" sizes="16x16">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.css">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/style.css">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/colors.css">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/responsive.css">
+	<meta name='author' content='xunny.id'>
+	<style>
+		p, li{
+			font-weight:500;
+			font-size:1.2rem;
+		}
+	</style>
+</head>
+<body>
+	<div id="pageWrapper">
+		<div class="phStickyWrap">
+			<header id="pageHeader" class="headerFixer" style='background-color:#4c3c29'>
+				<div class="container">
+					<div class="position-relative">
+						<div class="row">
+							<div class="col-6 col-md-3">
+								<div class="logo">
+									<a href="<?= site_url() ?>">
+										<img src="<?= base_url() ?>assets/images/logo.png" class="img-fluid" alt="Logo PT Alpha Konstruksi Nusantara">
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+		</div>
+		<main>
+			<section class="servicesBlock pt-12 pt-md-13 pt-lg-19 pb-lg-13 pt-xl-24 pb-xl-19" style='background-color:white'>
+				<div id="services" class="mt-n43 pt-43">
+					<div class="container">
+						<header class="text-center">
+							<h1 class="text-capitalize mb-4 mb-lg-7">Tentang Kami</h1>
+						</header>
+						<article class="text-dark wow fadeInUp" data-wow-delay="1s">
+							<div class="row justify-content-center">
+								<div class='col-12'><p>Jika mencari kontraktor bor Indonesia, Anda harus berkenalan dengan PT Alpha Konstruksi Nusantara sebagai ahlinya. PT Alpha Konstruksi Nusantara sendiri merupakan divisi pondasi dari sebuah perusahaan bernama PT Alpha Konstruksi Nusantara dan berdiri untuk pertama kali pada tahun 2016</p></div>
+								<div class="col-lg-6 col-md-4 col-sm-10 col-12">
+									<div class="embed-responsive embed-responsive-16by9">
+										<iframe class="w-100 embed-responsive-item" src="<?= base_url() ?>/assets/images/Project.mp4"></iframe>
+									</div>
+								</div>
+								<div class='col-lg-6 col-md-8 col-sm-10 col-12'>
+									<p>Mengingat prioritas utama presiden dua periode kita, Joko Widodo dalam hal pembangunan, maka Alpha Konstruksi Nusantara menjadi salah satu solusinya. Sang pendiri berpendapat bahwa dengan didirikannya perusahaan maka mendukung proses pembangunan oleh pemerintah maupun swasta.</p>
+								</div>
+								<div class='col-12'><p>Perusahaan yang khususnya menjalankan tugas sebagai spesialisasi pengeboran ini berlokasi di kota Kota Bekasi, Jawa Barat. Beberapa proyek yang dilakukan meliputi mini pile, bor pile, dan sebagainya. Dukungan alat modern dan tim solid merupakan kunci proses pekerjaan perusahaan.</p></div>
+								<div class='col-12'>
+									<h2 class='h4 mt-5'>Seputar PT Alpha Konstruksi Nusantara yang Perlu Diketahui</h2>
+									<p>Selayaknya sebuah perusahaan lain, Alpha Konstruksi Nusantara juga memiliki visi dan misi dalam menjalankan bisnis. Adapun visi yang dianut adalah</p>
+									<h3 class='h5 mt-3'>Visi Perusahaan</h3>
+									<p>Menjadi sebuah perusahaan konstruksi terdepan sekaligus inovatif di seluruh Indonesia dengan memanfaatkan teknologi mutakhir dan berkualitas. Segala proses tersebut dilakukan untuk mencapai kesejahteraan sekaligus kemajuan bangsa Indonesia dalam pembangunan.</p>
+									<h3 class='h5 mt-3'>Misi Perusahaan</h3>
+									<p>Sementara itu beberapa poin yang menjadi misi Alpha Konstruksi Nusantara meliputi:</p>
+									<ol>
+										<li>Berusaha membangun kepercayaan masyarakat melalui 4 hal, yakni norma, etika, kepedulian, dan kualitas terbaik.</li>
+										<li>Berusaha mengembangkan sumber daya manusia yang tangguh, berkualitas, inovatif, dan terus belajar untuk motivasi memajukan bangsa.</li>
+										<li>Berdiri pada tahun 2016 silam, Alpha Konstruksi Nusantara berharap mampu menjadi pelopor teknologi dalam dunia konstruksi.</li>
+										<li>Sebagai perusahaan kontraktor bor Indonesia, Alpha Konstruksi Nusantara akan berperan aktif serta maksimal dalam memberikan kontribusi, berupa pemikiran serta implementasi di bidang konstruksi bagi pemerintah, masyarakat, bahkan dunia internasional.</li>
+									</ol>
+									<p>Melalui visi dan misi tersebut, sebagai perusahaan terpercaya, Alpha Konstruksi Nusantara juga menganut motto berikut</p>
+									<h3>Motto Perusahaan</h3>
+									<p class='mb-5'>Perusahaan yang paling baik merupakan perusahaan dengan manfaat banyak terhadap masyarakat maupun bangsanya.</p>
+									<p>Motto yang diemban tentunya bukan hanya memikirkan keuntungan pribadi, namun juga mempertimbangkan keuntungan bagi pemerintah serta masyarakat, khususnya. Ini yang menjadi alasan kenapa perusahaan didirikan guna mendukung prioritas pembangunan oleh pemerintah.</p>
+									<p>Semakin banyak perusahaan kontraktor bor Indonesia terpercaya seperti Alpha Konstruksi Nusantara, maka semakin cepat tujuan mensejahterakan bangsa melalui infrastruktur tercapai. Selain perusahaannya harus berkualitas, tentu sumber daya manusianya juga wajib amanah.</p>
+								</div>
+								</article>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+		<div class="ftAreaWrap position-relative">
+			<footer id="pageFooter" class="bg-secondary pt-3 pb-1 position-relative text-center text-md-left wow fadeIn">
+				<div class="container">
+					<!-- ftNav -->
+					<nav class="ftNav">
+						<ul class="list-unstyled d-flex justify-content-center justify-content-md-start flex-wrap font-weight-normal text-uppercase mb-0">
+							<li>
+								<a href="<?= site_url() ?>" class="smooth">Beranda</a>
+							</li>
+							<li>
+								<a href="<?= site_url() ?>/#about" class="smooth">Tentang</a>
+							</li>
+							<li>
+								<a href="<?= site_url() ?>/#services" class="smooth">Jasa</a>
+							</li>
+							<li>
+								<a href="<?= site_url() ?>/#works" class="smooth">Pekerjaan</a>
+							</li>
+							<li>
+								<a href="<?= site_url() ?>/#client" class="smooth">Klien</a>
+							</li>
+							<li>
+								<a href="<?= site_url() ?>/#contact" class="smooth">Kontak</a>
+							</li>
+						</ul>
+					</nav>
+					<!-- textCopyright -->
+					<div class="textCopyright pt-4 d-md-none">
+						<p><strong class="fwMedium">&copy; <a href=="javascript:void(0);">SmithJohnson</a> 2020 | All Rights Reserved</strong></p>
+					</div>
+				</div>
+				<!-- btnTop -->
+				<a href="#pageWrapper" class="btnTop smooth fas fa-arrow-up d-none d-md-flex align-items-center justify-content-center bg-warning text-white position-absolute"><span class="sr-only">back to top</span></a>
+				<div id="loader" class="loader-holder">
+					<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+				</div>
+			</footer>
+		</div>
+	</div>
+	<script src="<?= base_url() ?>assets/js/jquery-3.4.1.min.js"></script>
+	<script src="<?= base_url() ?>assets/js/popper.min.js" async></script>
+	<script src="<?= base_url() ?>assets/js/bootstrap.min.js" async></script>
+	<script src="<?= base_url() ?>assets/js/jqueryCustom.js" async></script>
+	<script src="https://kit.fontawesome.com/391f644c42.js" async></script>
+</body>
+</html>
